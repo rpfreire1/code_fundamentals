c14c07ef007aeda145326aa0e3375de85ca3c8d0
Roberto Freire <rpfreire1@espe.edu.ec>
2025-01-29 16:56:18 -0500

    Api Versioning

--- src/main/java/com/rpfreire/git/test/controller/EmployeeController.java
+++ src/main/java/com/rpfreire/git/test/controller/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/v1/employee")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
